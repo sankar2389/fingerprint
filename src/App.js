15c26b87ad67a03d62f9e1fedcbd9212cec5c1c2
sankar2389 <sankar2389@gmail.com>
2022-05-09 19:05:38 +0530

    fingerprint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {DeviceUUID} from 'device-uuid';
 
 function App() {
+  const du = new DeviceUUID().parse();
+    let dua = [
+        du.language,
+        du.platform,
+        du.os,
+        du.cpuCores,
+        du.isAuthoritative,
+        du.silkAccelerated,
+        du.isKindleFire,
+        du.isDesktop,
+        du.isMobile,
+        du.isTablet,
+        du.isWindows,
+        du.isLinux,
+        du.isLinux64,
+        du.isMac,
+        du.isiPad,
+        du.isiPhone,
+        du.isiPod,
+        du.isSmartTV,
+        du.pixelDepth,
+        du.isTouchScreen
+    ];
+    let fingerprint = du.hashMD5(dua.join(':'));
+    console.log(fingerprint);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {fingerprint}
         </p>
         <a
           className="App-link"
